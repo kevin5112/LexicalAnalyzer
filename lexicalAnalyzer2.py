08121e37e87fefd1a2bcc53fed06aac9869b96ae
Kevin Chen <kevin5112@yahoo.com>
2020-09-14 18:55:15 -0700

    add feature to input file name to search for

--- lexicalAnalyzer2.py
+++ lexicalAnalyzer2.py
@@ -1,31 +1,44 @@
 import sys
 import re
+
 def main():
-	inFile = "inputTextFiles/sample2.txt"
+	# specify the input file needed for this program
+	directory = "inputTextFiles/"
+	inFile = "sample2.txt"
+	try:
+		userFile = input("input filename here(will draw from inputTextFiles directory): ")
+		with open(directory + userFile, 'r') as file:
+			lexicalAnalyzerTest(file)
+	except FileNotFoundError:
+		print("invalid file... will use default file\n")
+		with open(directory + inFile, 'r') as file:
+			lexicalAnalyzerTest(file)
 
-	with open(inFile, 'r') as file:
-		# changed it to pass as file
-		lexicalAnalyzerTest(file)
-
-
-# just made this function randomly to see if it works 
-# and I guess I finished making the project somehow
 def lexicalAnalyzerTest(file):
 	comments = "!"
 	keywords = {"int", "float", "bool", "true", "false", "if", "else", "then", "endif", "while", "whileend", "do", "doend", "for", "forend", "input", "output", "and", "or", "not"}
 	separators = {"'", "(", ")", "{", "}", "[", "]", ",", ".", ":", ";", "sp"}
 	operators = {"*","+","-","=","/",">","<","%"}
 	newStr = ""
+	print("TOKENS\t\t\tLexemes\n")
 	for line in file:
+		# skip for all comments
 		if comments in line:
 			continue
 		for letter in line:
+			# skip new lines and spaces
 			if letter == " " or letter == "\n":
 				continue
+			# check for non numeric and non alpha characters meaning it
+			# can't be a part of the identifiers unless it's a '$'
 			if not letter.isalpha() and not letter.isnumeric():
 				if newStr:
+					# check for '$' meaning it can still be an IDENTIFIER
 					if letter == "$":
 						newStr += letter
+					# will print IDENTIFIER here because once you reach a 
+					# non numeric and non alpha character, we will have a 
+					# string containing the identifier
 					print("IDENTIFIER\t=\t" + newStr)
 					newStr = ""
 				if letter in separators:
@@ -34,11 +47,10 @@ def lexicalAnalyzerTest(file):
 					print("OPERATOR\t=\t" + letter)
 			else:
 				if newStr in keywords:
+					# check to see if it's a reserved keyword
 					print("KEYWORD\t\t=\t" + newStr)
 					newStr = ""
 				newStr += letter
-
-
 
 if __name__ == "__main__":
 	main()
